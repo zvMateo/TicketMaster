15d6712d67f25a4590cef800cc7ec84640b97b76
zvMateo <zavalamateo14@gmail.com>
2024-08-15 18:16:15 -0300

    Iniciando proyecto de ticketmaster

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState, useRef } from 'react';
+
+import Navbar from './components/Navbar'
+import Events from './components/Events'
+import './App.css'
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const containerRef = useRef();
+
+  const handleNavbarSearch = (term) => {
+    console.log(containerRef.current.getterSomething);
+    setSearchTerm(term);
+  };
+
+
+  return (
+    <>
+      <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
+      <Events searchTerm={searchTerm}/>
+    </>
+  )
+}
+
+export default App
