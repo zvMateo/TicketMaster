52f96d9bca4679ddcdd12eba781a2c6c13775aad
zvMateo <zavalamateo14@gmail.com>
2024-08-15 18:43:39 -0300

    archivo que maneja la obtención, el almacenamiento y el manejo de errores de datos de eventos que se cargan desde un archivo JSON

---
+++ src/hooks/useEventsData.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import eventsJSON from '../data/events.json';
+
+
+const useEventsData = () => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState();
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            try {
+                setData(eventsJSON);
+                setIsLoading(false)
+            } catch (error) {
+                setError(error);
+            }
+            
+        }, 4000);
+        //* Load API Call 
+    }, []);
+
+    console.log(data);
+
+
+    return {
+        events:  data?._embedded?.events || [],
+        isLoading,
+        error,
+    };
+};
+
+export default useEventsData;
